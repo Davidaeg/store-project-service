904046552c477a0f2f1ad7a8e3f1f1f5b38218b9
SergioMongeC <56520392+SergioMongeC@users.noreply.github.com>
2024-03-10 23:26:22 -0600

    API REST FULL COLORS

---
+++ src/controllers/color.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import { ColorModel } from "../models/color/color.ts";
+import { validatePartialColor, validateColor } from "../schemas/color.ts";
+import ServerError from "@errors/ServerError.ts";
+import { ErrorsName, HTTP_STATUS } from "@errors/error.enum.ts";
+
+export class ColorController {
+    constructor(private colorModel: ColorModel) {}
+
+    getAll = async (_req: Request, res: Response) => {
+        const colors = await this.colorModel.getAll();
+        res.json(colors);
+    };
+
+    getById = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const color = await this.colorModel.getById({ id: Number(id) });
+        if (color) return res.json(color);
+        throw new ServerError({
+          name: ErrorsName.NotFoundException,
+          code: HTTP_STATUS.NOT_FOUND,
+          message: "Color not found",
+          logging: true,
+        });
+    };
+
+    create = async (req: Request, res: Response) => {
+        const result = validateColor(req.body);
+    
+        if (!result.success) {
+          throw new ServerError({
+            name: ErrorsName.UnprocessableEntityException,
+            code: HTTP_STATUS.UNPROCESSABLE_ENTITY,
+            message: JSON.parse(result.error.message),
+            logging: true,
+          });
+        }
+    
+        const newProduct = await this.colorModel.create(result.data);
+    
+        res.status(201).json(newProduct);
+      };
+
+      delete = async (req: Request, res: Response) => {
+        const { id } = req.params;
+    
+        const result = await this.colorModel.delete({ id: Number(id) });
+    
+        if (result === false) {
+          throw new ServerError({
+            name: ErrorsName.NotFoundException,
+            code: HTTP_STATUS.NOT_FOUND,
+            message: "Color not found",
+            logging: true,
+          });
+        }
+    
+        return res.json({ message: "Color deleted" });
+      };
+
+      update = async (req: Request, res: Response) => {
+        const result = validatePartialColor(req.body);
+    
+        if (!result.success) {
+          throw new ServerError({
+            name: ErrorsName.UnprocessableEntityException,
+            code: HTTP_STATUS.UNPROCESSABLE_ENTITY,
+            message: JSON.parse(result.error.message),
+            logging: true,
+          });
+        }
+    
+        const { id } = req.params;
+    
+        const updatedColor = await this.colorModel.update({
+          ...result.data,
+          colorId: Number(id),
+        });
+        return res.json(updatedColor);
+      };
+}
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { App } from "./app.ts";
 import { ProductModel } from "./models/product/product.ts";
 import { UsersModel } from "./models/user/user.ts";
 import { PersonModel } from "./models/person/person.ts";
+import { ColorModel } from "./models/color/color.ts";
 
 async function startApp() {
   const productModel = new ProductModel();
   const userModel = new UsersModel();
   const personModel = new PersonModel();
-  const app = new App(productModel, userModel, personModel);
+  const colorModel = new ColorModel();
+  const app = new App(productModel, userModel, personModel, colorModel);
   app.listen();
 }
 
